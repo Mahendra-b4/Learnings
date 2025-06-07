8b4fe203a06ae300553e95154c095d199a96bec3
Mahendra-b4 <mahendramudhiraj2529@gmail.com>
2025-06-07 22:57:09 +0530

    code

--- src/LeetCode/twoOutOfThree.java
+++ src/LeetCode/twoOutOfThree.java
@@ -1,16 +1,22 @@
 package LeetCode;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class twoOutOfThree {
     public static void main(String[] args) {
-        int[] nums1 = {1,1,3,2};
-        int[] nums2 = {2,3};
-        int[] nums3 = {3};
-        System.out.println(solver(nums1, nums2, nums3));
+//        int[] nums1 = {1,1,3,2};
+//        int[] nums2 = {2,3};
+//        int[] nums3 = {3};
+//        System.out.println(solver(nums1, nums2, nums3));
+
+        List<Integer> num = Arrays.asList(2,3,4,5);
+        num.stream().map(number -> number * number).forEach(System.out::println);
+
+        List<Integer> list = Arrays.asList(3, 6, 9, 12, 15);
+
+        // Using Stream map(Function mapper) and
+        // displaying the corresponding new stream
+        list.stream().map(number -> number * number).forEach(System.out::println);
     }
     public static List<Integer> solver(int[] nums1, int[] nums2, int[] nums3){
         int[] count = new int[101];
